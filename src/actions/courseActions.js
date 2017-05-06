fd40ebeb596752dff6188216ef13ce3ec6e564ce
tabasca <katu--shka@mail.ru>
2017-05-06 21:15:45 +0300

    react test app from pluralsight (cory house course)

---
+++ src/actions/courseActions.js
@@ -0,0 +1,39 @@
+import * as types from './actionTypes';
+import courseApi from '../api/mockCourseApi';
+import { beginAjaxCall, errorAjaxCall } from './ajaxStatusActions';
+
+export function loadCoursesSuccess(courses) {
+	return { type: types.LOAD_COURSES_SUCCESS, courses };
+}
+
+export function createCourseSuccess(courses) {
+	return { type: types.CREATE_COURSE_SUCCESS, courses };
+}
+
+export function updateCourseSuccess(courses) {
+	return { type: types.UPDATE_COURSE_SUCCESS, courses };
+}
+
+export function loadCourses() {
+	return function(dispatch) {
+		dispatch(beginAjaxCall());
+		return courseApi.getAllCourses().then(courses => {
+				dispatch(loadCoursesSuccess(courses));
+			}).catch(error => {
+				throw(error);
+		});
+	};
+}
+
+export function saveCourse(course) {
+	return function (dispatch, getState) {
+		dispatch(beginAjaxCall());
+		return courseApi.saveCourse(course).then(course => {
+			course.id ? dispatch(updateCourseSuccess(course)) :
+				dispatch(createCourseSuccess(course));
+		}).catch(error => {
+			dispatch(errorAjaxCall(error));
+			throw(error);
+		});
+	};
+}
---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+import {combineReducers} from 'redux';
+import courses from './courseReducer';
+import authors from './authorReducer';
+import numAjaxCallsInProgress from './ajaxStatusReducer';
+
+const rootReducer = combineReducers({
+	courses,
+	authors,
+	numAjaxCallsInProgress
+});
+
+export default rootReducer;
